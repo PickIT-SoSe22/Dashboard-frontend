5b8a88daafa64a8f22f50d1f987b78d10c3c40c3
Jannik Busch <jannikbusch-mail@web.de>
2022-04-29 12:02:40 +0200

    refactoring

---
+++ utils/mqttUtils.js
@@ -0,0 +1,28 @@
+import mqtt from "mqtt";
+
+export function mqttConnect () {
+    const options = {
+        keepalive: 60,
+        clientId: 'pick-it-dashboard',
+        protocolId: 'MQTT',
+        protocolVersion: 4,
+        clean: true,
+        reconnectPeriod: 1000,
+        connectTimeout: 30 * 1000,
+        will: {
+            topic: 'WillMsg',
+            payload: 'Connection Closed abnormally..!',
+            qos: 0,
+            retain: false
+        }
+    }
+    const uri = `ws://mqtt.castrumnubis.com:8083/mqtt`;
+
+    return mqtt.connect(uri, options);
+};
+
+export function mqttPublish (clientRef, topic, msg) {
+    if(clientRef) {
+        return clientRef.publish(topic, msg);
+    }
+};
