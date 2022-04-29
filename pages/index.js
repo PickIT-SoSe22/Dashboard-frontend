559025de41f3201e2e880dcc91709929252e5975
jannikbusch <59315264+jannikbusch@users.noreply.github.com>
2022-04-29 12:18:47 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,6 @@
 import Home from './home/Home.jsx'
 
-<<<<<<< Updated upstream
-export default function Home() {
-  const [client, setClient] = useState(null);
-
-  const mqttConnect = async () => {
-    const host = 'ws://mqtt.castrumnubis.com:8083/mqtt';
-    const options = {
-      keepalive: 60,
-      clientId: 'pick-it-dashboard',
-      protocolId: 'MQTT',
-      protocolVersion: 4,
-      clean: true,
-      reconnectPeriod: 1000,
-      connectTimeout: 30 * 1000,
-      will: {
-        topic: 'WillMsg',
-        payload: 'Connection Closed abnormally..!',
-        qos: 0,
-        retain: false
-      },
-    }
-    setClient(await mqtt.connect(host, options));
-  };
-
-  const mqttPublish = async () => {
-    console.log('pub', client);
-    if(client) {
-      await client.publish("test/2", "hello pickit!");
-    }
-  };
-
-  useEffect(() => {
-    if (client) {
-      console.log(client)
-      client.on('error', (err) => {
-        console.log('Connection error: ', err)
-        client.end()
-      })
-    }
-  }, [client]);
-
-  return (
-    <div>
-      <h1>ESP-32 Dashboard</h1>
-      <LightDevice />
-      <Button onClick={() => mqttConnect()}>Connect</Button>
-      <Button onClick={() => mqttPublish()}>Test</Button>
-
-    </div >
-  )
-=======
 export default function index() {
   // Hier routing?
   return <Home />;
->>>>>>> Stashed changes
 }
